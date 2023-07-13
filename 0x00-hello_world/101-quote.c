a5b88fc6a89fe1af3c6d0cf503ddcbf6d3132bc2
TUYISHIMIRE Hilbert <hilbert.apply@gmail>
2023-07-13 12:35:22 -0700

    101-quotes.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,11 +8,7 @@
  */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	ssize_t len;
-
-	len = syscall(SYS_write, STDERR_FILENO, message, 59);
-	(void) len;
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n,stdout");
 
 	return (1);
 }
