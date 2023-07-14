c8a86925ac49f256a1afda6bc1a533d745347ff0
TUYISHIMIRE Hilbert <hilbert.apply@gmail>
2023-07-14 12:23:46 -0700

    101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,27 +15,27 @@
  */
 int main(void)
 {
-    int i, j, k;
+	int i, j, k;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (j = i + 1; j < 10; j++)
-        {
-            for (k = j + 1; k < 10; k++)
-            {
-                putchar(i + '0');
-                putchar(j + '0');
-                putchar(k + '0');
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				putchar(',');
+				putchar(' ');
 
-    putchar('\b');
-    putchar('\b');
-    putchar('\n');
+				/* Check if last combination reached */
+				if (i == 9 && j == 8 && k == 7)
+					putchar('\n');
+			}
+		}
+	}
 
-    return (0);
+	return (0);
 }
 
