9450c010b484a01a173e990c04a4a4e6410ad5de
TUYISHIMIRE Hilbert <hilbert.apply@gmail>
2023-07-14 11:13:54 -0700

    3 changes

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,5 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <limits.h>
-
 /**
  * main - Entry point of the program
  *
@@ -13,20 +10,10 @@
  */
 int main(void)
 {
-    int n;
+	int n;
 
-    srand(time(0)); /* Seed the random number generator with the current time */
-
-    n = rand() % (INT_MAX - INT_MIN + 1) + INT_MIN; /* Assign a random number to n */
-
-    printf("The number %d is ", n);
-
-    if (n > 0)
-        printf("positive\n");
-    else if (n < 0)
-        printf("negative\n");
-    else
-        printf("zero\n");
-
-    return 0;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
 }
---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the alphabet in lowercase, followed by a new line,
+ *              using the putchar function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
+}
