ed63ae7de1bef12b0af157e6d8374c5aa303771f
TUYISHIMIRE Hilbert <hilbert.apply@gmail>
2023-07-14 10:40:40 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,20 +12,20 @@
  */
 int main(void)
 {
-	int n;
+    int n;
 
-	srand(time(0)); /* Seed the random number generator with the current time */
+    srand(time(0)); /* Seed the random number generator with the current time */
 
-	n = rand() % (2 * RAND_MAX) - RAND_MAX; /* Assign a random number to n */
+    n = rand() % (2 * RAND_MAX + 1) - RAND_MAX; /* Assign a random number to n */
 
-	printf("The number %d is ", n);
+    printf("The number %d is ", n);
 
-	if (n > 0)
-		printf("positive\n");
-	else if (n < 0)
-		printf("negative\n");
-	else
-		printf("zero\n");
+    if (n > 0)
+        printf("positive\n");
+    else if (n < 0)
+        printf("negative\n");
+    else
+        printf("zero\n");
 
-	return 0;
+    return 0;
 }
