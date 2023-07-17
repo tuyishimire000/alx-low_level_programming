fc9a39533528a7b0f503a35507635df6047f0aec
TUYISHIMIRE Hilbert <hilbert.apply@gmail>
2023-07-17 13:12:46 -0700

    matching desired output format

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,13 +14,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("%d",n);
 	if (n>0){
-		printf("is positive");
+		printf("%d",n,"is positive");
 	}else if(n==0){
-	        printf("is zero");
+	        printf("%d",n,"is zero");
 	}else{
-	        printf("is negative");
+	        printf("%d",n,"is negative");
 	}
 	return (0);
 }
