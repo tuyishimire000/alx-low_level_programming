e3260a2ae6b98762089b7969051a40be1ec3c454
TUYISHIMIRE Hilbert <hilbert.apply@gmail>
2023-07-18 12:12:55 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - check whter valur is lower or not
+ * @c: int value
+ * Description: return 1 if condition is true
+ * Return: int value
+ **/
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
