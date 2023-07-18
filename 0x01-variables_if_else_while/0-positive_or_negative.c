d276fd2122d0cdbfcb338815bff5c24364d42b6e
TUYISHIMIRE Hilbert <hilbert.apply@gmail>
2023-07-18 05:57:22 -0700

    fixing betty coding style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,9 +16,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("%d",n);
-	if (n>0){
+	if (n > 0){
 		printf("is positive");
-	}else if(n==0){
+	}else if(n == 0){
 	        printf("is zero");
 	}else{
 	        printf("is negative");
