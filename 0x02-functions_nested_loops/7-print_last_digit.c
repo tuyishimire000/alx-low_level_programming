7e09563e1781c8366204ef8493a63ebd7ea31262
TUYISHIMIRE Hilbert <hilbert.apply@gmail>
2023-07-18 13:59:27 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,11 +5,14 @@
  * Description: divide number by 10 to get the last digit as a remainder
  * Return: int  value
  */
-#include <stdio.h>
-
 int print_last_digit(int num)
 {
-int last_digit = num % 10;
-_putchar(last_digit);
+int last_digit;
+if (num < 0)
+{
+num = num * (-1);
+}
+last_digit = num % 10;
+_putchar (last_digit);
 return (last_digit);
 }
