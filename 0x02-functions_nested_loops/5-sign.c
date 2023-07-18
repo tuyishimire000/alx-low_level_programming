3d2f31ce572f70b7ec03e214332f6d3dc55b5aee
TUYISHIMIRE Hilbert <hilbert.apply@gmail>
2023-07-18 13:09:26 -0700

    putchar('-');

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-putchar('+');
 return (1);
+putchar('+');
 }
 else if (n == 0)
 {
-putchar('0');
 return (0);
+putchar('0');
 }
 else
 {
-putchar('-');
 return (-1);
+putchar('-');
 }
 }
