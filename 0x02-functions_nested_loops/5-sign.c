cea9ac59dbe16dc3c24bbbe6e08476e717f3e019
TUYISHIMIRE Hilbert <hilbert.apply@gmail>
2023-07-18 12:45:47 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_sign - print the sign of number
+ * @n: int value to be checked
+ * Description: show if it's negative/positive or zero
+ * Return: int  value
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+putchar('+');
+putchar(',');
+putchar(' ');
+return (1);
+}
+else if (n == 0)
+{
+putchar('0');
+putchar(',');
+putchar(' ');
+return (0);
+}
+else
+{
+putchar('-');
+putchar(',');
+putchar(' ');
+return (-1);
+}
+}
