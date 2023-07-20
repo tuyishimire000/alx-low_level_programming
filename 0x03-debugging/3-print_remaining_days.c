74797701d366a7dd4c2f402763a375ebd3b59127
TUYISHIMIRE Hilbert <hilbert.apply@gmail>
2023-07-20 11:51:39 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,21 +10,29 @@
 */
 int is_leap_year(int year)
 {
-return ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
+return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 }
 void print_remaining_days(int month, int day, int year)
 {
-int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 if (is_leap_year(year))
 {
-days_in_month[2] = 29; // Update February to have 29 days in a leap year
-}
-int day_of_year = 0;
-for (int i = 1; i < month; i++)
+if (month > 2 || (month == 2 && day >= 29))
 {
-day_of_year += days_in_month[i];
+day++;
 }
-day_of_year += day;
-printf("Day of the year: %d\n", day_of_year);
-printf("Remaining days: %d\n", is_leap_year(year) ? 366 - day_of_year : 365 - day_of_year);
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 366 - day);
+}
+else
+{
+if (month == 2 && day == 29)
+{
+printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+}
+else
+{
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 365 - day);
+}
+}
 }
